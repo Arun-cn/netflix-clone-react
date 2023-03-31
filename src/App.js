15cd4f7ed480f5d8e5174988e2e5f1c44393e224
Arun Chandran <53257113+arunchandran409@users.noreply.github.com>
2023-03-31 11:00:57 +0530

    add rowpost

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
      <Banner/>
-     
-      
-     
+     <RowPost/>     
     </div>
   );
 }
